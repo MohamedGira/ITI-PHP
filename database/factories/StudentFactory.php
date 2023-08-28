5306716857752a74a6cb0a540dd7aab73f0a3079
MohamedGira <mohamedgira0901@gmail.com>
2023-08-28 15:03:01 +0300

    added factories/seeders

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name'=>fake()->name(),
+            'email'=>fake()->email(),
+            'Bdate'=>fake()->date(),
+            'address'=>fake()->address(),
+            'track'=>fake()->randomElement(['web','embedded','mobile','design'])
+        ];
+    }
+}
