a2e0bef734c19b1bd4e242eef430ede13da17cf3
MohamedGira <mohamedgira0901@gmail.com>
2023-08-31 13:13:19 +0300

    added api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+
+use App\Http\Controllers\Api\StudentController;
+use App\Http\Controllers\Api\CourseController;
+use App\Http\Controllers\Api\EnrollmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/students',[StudentController::class,'index'])->name('student.index');
+Route::post('/students',[StudentController::class,'store'])->name('student.store');
+Route::get('/students/{id}',[StudentController::class,'show'])->name('student.show');
+Route::patch('/students/{id}',[StudentController::class,'update'])->name('student.update');
+Route::delete('/students/{id}',[StudentController::class,'destroy'])->name('student.delete');
+
+Route::get('/courses',[CourseController::class,'index'])->name('course.index');
+Route::get('/courses/{id}',[CourseController::class,'show'])->name('course.show');
+
+
+Route::get('/courses/{id}/students',[CourseController::class,'getStudents'])->name('course.showStudents');
+Route::get('/students/{id}/my-courses',[StudentController::class,'myCourses'])->name('student.myCourses');
+
+Route::get('/enrollments',[EnrollmentController::class,'index'])->name('course.index');
+Route::post('/enrollments',[EnrollmentController::class,'store'])->name('student.store');
